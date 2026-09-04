15de2fe19535a6395d6df4f71084db1bb88524da
carlosjunco2025 <carlos.junco@tecsup.edu.pe>
2026-09-04 15:16:45 -0500

    Agrega encabezado con jerarquia tipografica

---
+++ lab03/app/src/main/java/com/juncolab03/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.juncolab03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.juncolab03.ui.theme.Lab03Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab03Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PantallaRegistro(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaRegistro(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        //  FTextos del encabezado con jerarquía tipográfica
+        Text(
+            text = "Nuevo producto",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = "Completa los datos y presiona Agregar",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
